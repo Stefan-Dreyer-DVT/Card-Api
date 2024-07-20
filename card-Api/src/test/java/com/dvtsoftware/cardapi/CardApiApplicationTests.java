a98d76cdba6ec80ceafab53452a7d72b82a2c9a7
Stefan <SDreyer@dvtsoftware.com>
2024-07-20 11:57:06 +0200

    Initial Commit (setup)

---
+++ card-Api/src/test/java/com/dvtsoftware/cardapi/CardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dvtsoftware.cardapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
